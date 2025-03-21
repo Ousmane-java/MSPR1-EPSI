1784bcbeb7045954ef62b788e05a82541a032893
Mouad EL'G <154380659+ELGOURDI200@users.noreply.github.com>
2025-03-21 16:36:48 +0100

    j'ai modifie le fichier config.py

--- nester/config.py
+++ nester/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'devkey')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
